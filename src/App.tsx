8893390ef34f8858ba915ddaa9fc6e09fbabdba0
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-09 11:28:36 -0600

    updated App to use the proper pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Layout from "./pages/Layout"
+import PageNotFound from "./pages/PageNotFound"
+import Dashboard from "./pages/Dashboard"
 
 const App = () => {
 
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
     </>
   )
 }
