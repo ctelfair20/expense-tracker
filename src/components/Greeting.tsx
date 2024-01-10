f448b9b75b7a0153cb4c5d719c8f7024592f91c5
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-10 13:15:39 -0600

    removed inline styles of all typographys

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from '@mui/material';
 function Greeting() {
   return (
     <Box sx={{}}>
-      <Typography variant='h4' component='h2' sx={{ color: '#613F75' }}>
+      <Typography variant='h4' component='h2'>
         {/* I want to dynamically change "morning" to either "afternoon" or "evening" and show the currently logging in user's name*/}
         Good Morning User!
       </Typography>
