0702a7a0ca10394247a59e1baef56b89b1b44986
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-10 12:28:57 -0600

    added comments

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -2,9 +2,9 @@ import { Box, Typography } from '@mui/material';
 
 function Greeting() {
   return (
-    <Box>
+    <Box sx={{}}>
       <Typography variant='h4' component='h2' sx={{ color: '#613F75' }}>
-        {/* I want to dynamically change "morning" to either "afternoon" or "evening" */}
+        {/* I want to dynamically change "morning" to either "afternoon" or "evening" and show the currently logging in user's name*/}
         Good Morning User!
       </Typography>
     </Box>
