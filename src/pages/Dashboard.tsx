b035ea050af7eac16a40a825dcfa99b53b6c890d
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-10 12:28:32 -0600

    added padding

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 import Greeting from '../components/Greeting';
 
 const Dashboard = () => {
-  return <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+  return <Box sx={{ display: 'flex', justifyContent: 'center', p: 3 }}>
     <Greeting />
   </Box>;
 }
