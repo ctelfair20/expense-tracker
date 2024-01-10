9e0367b84e085b036c4cc5c86c5127416eb7e166
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-09 20:15:35 -0600

    installed mui, styled Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router-dom"
+import { Box, Avatar, AppBar, Toolbar, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <AppBar position="static" sx={{ display: "flex", boxShadow: "none", height: "100px", width: "100%", p: 2, background: "#F0EFF4" }}>
+      <Toolbar sx={{ display: "flex", gap: 2, justifyContent: "space-between" }}>
+        <Box>
           <Link to="/">Home</Link>
-        </li>
-        <li>
           <Link to="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link to="/nothing-here">Nothing Here</Link>
-        </li>
-      </ul>
-    </nav>
+        </Box>
+        <Box sx={{ display: "flex" }}>
+          <Avatar sx={{ color: "#613F75", background: "none", border: "#613F75 2px solid", width: "50px", height: "50px" }} />
+          <IconButton>
+            <MenuIcon sx={{ width: "30px", height: "30px", color: "#613F75" }} />
+          </IconButton>
+        </Box>
+      </Toolbar>
+
+
+
+    </AppBar>
   )
 }
 
