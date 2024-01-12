f539e22d2a3108a7dfc0467455373a08dd16bb58
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-12 11:21:06 -0600

    removed the hover effect over plus icon

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,4 +1,6 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
 
 type Props = {
   name: string,
@@ -7,10 +9,19 @@ type Props = {
 
 function CustomButton({ name, onClick }: Props) {
   return (
-    <Button sx={{ border: "solid #613F75 2px", width: '150px' }} onClick={onClick}>
+    <Button sx={{ border: "solid #613F75 2px", width: '200px', height: '40px', m: 1 }} onClick={onClick}>
       <Typography>
         {name}
       </Typography>
+      <IconButton sx={{
+        color: "#613F75",
+        pr: 0,
+        "&.MuiButtonBase-root:hover": {
+          bgcolor: "transparent"
+        }
+      }}>
+        <AddIcon sx={{ pr: 0 }} />
+      </IconButton>
     </Button>
   )
 }
