e8faf04312ac4c2dd9592cd8db2e7005cd91de51
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-11 23:30:45 -0600

    organize dashboard into two columns

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,31 @@
 import { Box } from '@mui/material';
 import Greeting from '../components/Greeting';
 import CustomButton from '../components/CustomButton';
+import { addCategory, addExpense } from '../handler functions/buttons';
 
 const Dashboard = () => {
-  return <Box sx={{ display: 'flex', justifyContent: 'center', p: 3 }}>
-    <Greeting />
-    <CustomButton name='addExpene +' />
-  </Box>;
+  return (
+    // dashboard - under header
+    <Box sx={{ display: 'flex', flexDirection: 'column', p: 3, backgroundColor: 'blue' }}>
+      <Greeting />
+      {/* under greeting */}
+      <Box sx={{ backgroundColor: 'pink', width: '100%', display: 'flex' }}>
+
+        {/* categories column*/}
+        <Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', backgroundColor: 'red' }}>
+            <CustomButton name='addExpene +' onClick={addExpense} />
+            <CustomButton name='addCategory +' onClick={addCategory} />
+          </Box>
+          <Box sx={{ backgroundColor: 'purple', }}>categories</Box>
+        </Box>
+
+        {/* budgets column*/}
+        <Box sx={{ backgroundColor: 'purple', }}>budgets</Box>
+
+      </Box>
+    </Box>
+  );
 }
 
 export default Dashboard;
