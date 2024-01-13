be04fcd2408cd28af1e2c2b8cad4a1e4e7bd52d2
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-13 15:52:36 -0600

    updated category

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,21 +1,27 @@
 import { Box, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import Expense from './Expense';
+import CustomButton from './CustomButton';
+import { addExpense } from '../handler-functions/buttons';
 
 function Category() {
   return (
     <Box>
+      <Box sx={{ display: 'flex', gap: 2, p: 1 }}>
+        <Typography variant='h5' component='h3' sx={{ textDecoration: 'underline' }}>
+          Groceries
+        </Typography>
+        <Typography variant='h5' component='h3'>
+          $78
+        </Typography>
+      </Box>
       <Box>
-        <Box sx={{ display: 'flex', gap: 2, p: 1 }}>
-          <Typography variant='h5' component='h3' sx={{ textDecoration: 'underline' }}>
-            Groceries
-          </Typography>
-          <Typography variant='h5' component='h3'>
-            $78
-          </Typography>
-        </Box>
         <Expense />
       </Box>
-    </Box >
+      <Box sx={{ background: 'green', display: 'flex', justifyContent: 'flex-end' }}>
+        <CustomButton name='Add Expense' width='200px' icon={<AddIcon sx={{ pl: 2, color: "#613F75" }} />} onClick={addExpense} />
+      </Box>
+    </Box>
   )
 }
 
