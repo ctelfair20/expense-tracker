19e8d1ec81ef922b43000475735424357d7e7a9e
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-13 21:34:32 -0600

    added styles to budgetTable

--- src/components/BudgetTable.tsx
+++ src/components/BudgetTable.tsx
@@ -1,10 +1,28 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
 import Budget from "./Budget"
+import CustomButton from './CustomButton'
+import { addBudget } from '../handler-functions/buttons'
 
 const BudgetTable = () => {
   return (
-    <Box>BudgetTable
-      <Budget />
+    <Box>
+      <Box sx={{ display: 'flex', gap: 2, m: 0.5 }}>
+        <Typography variant='h5' component='h3' sx={{ textDecoration: 'underline' }}>
+          Budget Totals:
+        </Typography>
+        <Typography variant='h5' component='h3'>
+          $1000
+        </Typography>
+      </Box>
+      <Box sx={{ my: 3 }}>
+        <Budget />
+        <Budget />
+        <Budget />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <CustomButton name='Add Budget' width='200px' icon={<AddIcon sx={{ pl: 2, color: "#613F75" }} />} onClick={addBudget} />
+      </Box>
     </Box>
 
   )
