7d98d3112f334d7a80c5bc52ec5333fa88218de0
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-13 21:33:08 -0600

    created basic budget

--- src/components/Budget.tsx
+++ src/components/Budget.tsx
@@ -1,8 +1,30 @@
-import { Box } from "@mui/material";
+import { Box, IconButton, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import CustomButton from './CustomButton';
+import { editCost } from '../handler-functions/buttons';
 
 const Budget = () => {
   return (
-    <Box>Budget</Box>
+    <Box sx={{ display: 'flex', borderBottom: '2px solid #613F75', alignItems: 'center', justifyContent: 'space-between', gap: 1, mb: 1 }}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        {/* close button */}
+        <IconButton sx={{
+          color: "#613F75"
+        }}>
+          <CloseIcon fontSize='small' />
+        </IconButton>
+        {/* budget name */}
+        <Typography>
+          Groceries
+        </Typography>
+      </Box>
+
+      {/* cost */}
+      <Typography>
+        $300
+      </Typography>
+      <CustomButton name='Edit' width='70px' onClick={editCost}></CustomButton>
+    </Box>
   );
 }
 
