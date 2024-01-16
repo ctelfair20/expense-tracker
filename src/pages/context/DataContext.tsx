c7999fae488d3fa606085f5790e9fbad345cb2fa
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-16 14:20:50 -0600

    typed dataContext

--- src/pages/context/DataContext.tsx
+++ src/pages/context/DataContext.tsx
@@ -1,5 +1,40 @@
+import { createContext } from "react";
+
+type Budget = {
+  id: number,
+  name: string,
+  total: number,
+}
+
+type Expense = {
+  id: number,
+  category: string | "Other",
+  name: string,
+  cost: number,
+  purchasedDate: Date
+}
+
+type Category = {
+  id: number,
+  name: string | "Other",
+  expenses: Expense[]
+}
+
+type DashboardData = {
+  categories: Category[],
+  budgets: Budget[]
+}
+
 const DataProvider = () => {
-  return (<></>);
+  const dataContext = createContext<DashboardData | null>(null)
+
+
+
+
+
+  return (
+    <></>
+  );
 }
 
 export default DataProvider;
