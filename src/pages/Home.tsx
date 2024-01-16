2fb623bb841dac0e6adfd0476cca571c0648293d
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-16 13:12:29 -0600

    added basic login form to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import { Box, TextField, Typography } from "@mui/material";
+
 const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <Box sx={{ m: 2, border: '2px solid black', width: '100%', height: '100%' }}>
+      <Typography>Home</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '300px', border: '2px solid black', m: 1, p: 1 }}>
+        <TextField sx={{ mb: 1 }} id="login" label="login" />
+        <TextField sx={{ mb: 1 }} id="password" label="password" />
+      </Box>
+    </Box>
+  )
 }
 
 export default Home;
