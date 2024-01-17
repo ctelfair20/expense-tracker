c522ef13288f593c58bf142003da32d2d272c466
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-17 13:13:04 -0600

    used DataProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { createTheme, ThemeProvider } from "@mui/material"
+import { createTheme, ThemeProvider } from '@mui/material'
 import App from './App.tsx'
+import DataProvider from './context/DataProvider.tsx'
 import './index.css'
 
 const theme = createTheme({
@@ -16,9 +17,11 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <DataProvider>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </DataProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
