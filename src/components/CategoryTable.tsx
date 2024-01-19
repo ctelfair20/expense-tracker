9dfb35a5692fbbbec9eea9d8b3a97694e074700a
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-18 20:54:13 -0600

    use placeholder to display categories and expenses

--- src/components/CategoryTable.tsx
+++ src/components/CategoryTable.tsx
@@ -3,25 +3,35 @@ import AddIcon from '@mui/icons-material/Add';
 import Expense from './Expense';
 import CustomButton from './CustomButton';
 import { addExpense } from '../handler-functions/buttons';
+import { categories } from '../assets/placeholderData';
 
 function CategoryTable() {
+
+  const displayAllCategories = categories.map((category) => {
+    return (
+      <Box key={category.id}>
+        <Box sx={{ display: 'flex', gap: 2, p: 1 }}>
+          <Typography variant='h5' component='h3' sx={{ textDecoration: 'underline' }}>
+            {category.name}:
+          </Typography>
+          <Typography variant='h5' component='h3'>
+            $78
+          </Typography>
+        </Box>
+        <Box>
+          {category.expenses.map(expense => {
+            return <Expense name={expense.name} cost={expense.cost} key={expense.id} />
+          })}
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <CustomButton expense='Add Expense' width='180px' icon={<AddIcon sx={{ pr: 1, color: "#613F75" }} />} onClick={addExpense} />
+        </Box>
+      </Box>
+    )
+  });
+
   return (
-    <Box>
-      <Box sx={{ display: 'flex', gap: 2, p: 1 }}>
-        <Typography variant='h5' component='h3' sx={{ textDecoration: 'underline' }}>
-          Groceries:
-        </Typography>
-        <Typography variant='h5' component='h3'>
-          $78
-        </Typography>
-      </Box>
-      <Box>
-        <Expense />
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <CustomButton expense='Add Expense' width='180px' icon={<AddIcon sx={{ pr: 1, color: "#613F75" }} />} onClick={addExpense} />
-      </Box>
-    </Box>
+    displayAllCategories
   )
 }
 
