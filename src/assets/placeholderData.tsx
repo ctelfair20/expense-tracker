17a683fd3baa40e15ba8782b2ff731cc9b350aa7
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-18 18:30:16 -0600

    update placeholder data

--- src/assets/placeholderData.tsx
+++ src/assets/placeholderData.tsx
@@ -12,24 +12,39 @@ export const budgets = [
   // Add more budgets as needed
 ];
 
-export const category = {
-  id: 1,
-  name: "Example Category",
-  expenses: [
-    {
-      id: 101,
-      category: "Food",
-      name: "Groceries",
-      cost: 50.0,
-      purchasedDate: new Date("2024-01-16"),
-    },
-    {
-      id: 102,
-      category: "Utilities",
-      name: "Electricity Bill",
-      cost: 80.0,
-      purchasedDate: new Date("2024-01-15"),
-    },
-    // Add more expenses as needed
-  ],
-};
+export const categories = [
+  {
+    id: 1,
+    name: 'Food',
+    expenses: [
+      {
+        id: 101,
+        category: "Food",
+        name: "Groceries",
+        cost: 50.0,
+        purchasedDate: new Date("2024-01-16"),
+      },
+    ]
+  },
+
+  {
+    id: 2,
+    name: 'Utilities',
+    expenses: [
+      {
+        id: 102,
+        category: "Utilities",
+        name: "Electricity Bill",
+        cost: 80.0,
+        purchasedDate: new Date("2024-01-15"),
+      },
+      {
+        id: 103,
+        category: "Utilities",
+        name: "Gas Bill",
+        cost: 50.0,
+        purchasedDate: new Date("2024-01-15"),
+      },
+    ]
+  }
+];
