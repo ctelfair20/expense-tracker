765d763089157a3f422f4c13f62d86ae6c7aef74
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2024-01-18 20:48:55 -0600

    working on total func

--- src/handler-functions/buttons.tsx
+++ src/handler-functions/buttons.tsx
@@ -1,3 +1,5 @@
+// import { Budget, Expense } from "../context/DataContext";
+
 const addExpense = () => {
 
 };
@@ -17,4 +19,34 @@ const addBudget = () => {
 const editCost = () => {
 
 }
+
+// helper functions
+// const getTotal1 = (arr: Budget[]) => {
+//   const initialValue = 0;
+//   return arr.reduce(
+//     (accumulator, currentValue) => accumulator + currentValue.total,
+//     initialValue,
+//   );
+// }
+
+// const getTotal = (arr: Expense[] | Budget[]) => {
+//   const initialValue = 0;
+
+//   if('cost' in arr) {
+//     return arr.reduce(
+//       (accumulator, currentValue) => {
+//         return accumulator + currentValue.cost
+//       }
+//     ),initialValue
+//   }
+// }
+
+// const totalCB = (accumulator:number, currentValue: Expense[] | Budget[]) => {
+//   if('cost' in currentValue) {
+//     return accumulator + currentValue.cost
+//   } else {
+//     return accumulator + currentValue.total
+//   }
+// }
+
 export { addExpense, addCategory, deleteBudget, addBudget, editCost }
